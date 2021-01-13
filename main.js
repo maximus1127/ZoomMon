7dd87f3cac99d13ff776d5b4aeceae288964d60e
Emploi <developerjasper@gmail.com>
2021-01-13 17:16:16 +0100

    Modification

--- main.js
+++ main.js
@@ -1,59 +1,42 @@
-const { app, BrowserWindow, Tray, Menu } = require('electron')
-var ipcMain = require('electron').ipcMain;
-const Store = require('electron-store');
-const store = new Store();
-const fs = require('fs');
+"use strict";
 
-shouldScan = true;
+const {app, nativeImage, Tray, Menu, BrowserWindow} = require("electron");
+const path = require('path');
+let config = {};
 
-// global window declaration function
-var win;
-async function createWindow () {
-    win = new BrowserWindow({
-    width: 500,
-    height: 250,
-    webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule: true,
-    }
-  })
-  win.setMenuBarVisibility(false)
-  win.loadFile('index.html')
+app.once("ready", ev => {
+    config.win = new BrowserWindow({
+        width: 500, height: 250, center: true, minimizable: false, show: true,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            webSecurity: true,
+            sandbox: true,
+        },                                
+    });
+    // win.setMenuBarVisibility(false)
+    config.win.loadFile("index.html");
+    config.win.on("close", ev => {
+        ev.sender.hide();
+        ev.preventDefault(); 
+    });
 
-  tray = new Tray('spongebob.ico')
-  const contextMenu = Menu.buildFromTemplate([
-    {
-             label: 'Show App', click: function () {
-                 win.show()
-             }
-         },
-         {
-             label: 'Quit', click: function () {
-                 app.isQuiting = true
-                 app.quit()
-             }
-         }
-  ])
-  tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
+    config.tray = new Tray(path.join(__dirname, 'asset','img', 'icon_16x16.png'));
+    const menu = Menu.buildFromTemplate([
+            {label: "ShowApp", click: (item, window, event) => {
+                config.win.show();
+            }},
 
-  win.on('minimize', function (event) {
-    event.preventDefault()
-    shouldScan = true
-    scanning()
-    win.hide()
-})
+        {type: "separator"},
+        {role: "quit"}, 
+    ]);
 
-win.on('show', function (event) {
-  event.preventDefault()
-  shouldScan = false
-})
-await sleep(1000)
-win.minimize()
-}
+    config.tray.setToolTip("This is my application.");
+    config.tray.setTitle("ZoomMon"); // macOS only
+    config.tray.setContextMenu(menu);
 
-// start the application
-app.whenReady().then(createWindow)
+});
+
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -68,39 +51,8 @@ app.on('activate', () => {
 })
 
 
-
-
-//allow delays
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
-//check the designated folder and stop the loop while showing the app window from the tray
-async function scanning(){
-  while(shouldScan){
-    console.log('scanning')
-    if(store.get('default_path') != null){
-      files = fs.readdirSync(store.get('default_path'));
-      if(files.length > 0){
-        fs.rename(store.get('default_path') + "/" + files[0], store.get('default_path') + "/encounter", err => {
-        if (err) {
-          console.error(err)
-          return
-        }
-        })
-        console.log('should have shown')
-        win.show()
-        shouldScan = false
-    }
-  }
-    await sleep(1000)
-  }
-}
-
-//start the scanning funciton again when the signal is received
-ipcMain.on('processInput', function(event, status) {
-  win.hide()
-  shouldScan = true
-  scanning()
+app.on("before-quit", ev => {
+    config.win.removeAllListeners("close");
+    config = null;
 });
+
